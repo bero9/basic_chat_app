2cc9ff72bf8ccb0ad9a3c3aeb056ce497900df48
bero9 <berohamood545@gmail.com>
2024-03-20 12:11:31 +0300

    fix chat bublle

--- lib/components/custom_text_bubble.dart
+++ lib/components/custom_text_bubble.dart
@@ -10,23 +10,25 @@ class CustomTextBubble extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Align(
       alignment: Alignment.centerLeft,
-      padding: EdgeInsets.only(left: 16),
-      height: 65,
-      width: 150,
-      margin: EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(32),
-            topRight: Radius.circular(32),
-            bottomRight: Radius.circular(32)
+      child: Container(
+
+        padding: EdgeInsets.only(left: 16,right: 24,bottom: 24,top: 24),
+
+        margin: EdgeInsets.symmetric(vertical: 8,horizontal: 16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(32),
+              topRight: Radius.circular(32),
+              bottomRight: Radius.circular(32)
+          ),
+          color: KPrimaryColor,
         ),
-        color: KPrimaryColor,
+        child: Text('I am a new user',style: TextStyle(
+          color: Colors.white,
+        ),),
       ),
-      child: Text('I am a new user',style: TextStyle(
-        color: Colors.white,
-      ),),
     );
   }
 }
