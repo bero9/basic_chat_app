dcf7e6325fd77f6c84325c91f3316818f3ce4ec5
bero9 <berohamood545@gmail.com>
2024-03-21 09:55:16 +0300

    add new chat  bubble to second user and show message.message

--- lib/components/custom_text_bubble.dart
+++ lib/components/custom_text_bubble.dart
@@ -2,11 +2,13 @@
 import 'package:flutter/material.dart';
 
 import '../Constants.dart';
+import '../models/messges.dart';
 
 class CustomTextBubble extends StatelessWidget {
   const CustomTextBubble({
-    super.key,
+    super.key, required this.message,
   });
+ final Message message;
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +27,41 @@ class CustomTextBubble extends StatelessWidget {
           ),
           color: KPrimaryColor,
         ),
-        child: Text('I am a new user',style: TextStyle(
+        child: Text(message.message,style: TextStyle(
+          color: Colors.white,
+        ),),
+      ),
+    );
+  }
+}
+
+
+
+
+class CustomTextBubble2 extends StatelessWidget {
+  const CustomTextBubble2({
+    super.key, required this.message,
+  });
+  final Message message;
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: Container(
+
+        padding: EdgeInsets.only(left: 16,right: 24,bottom: 24,top: 24),
+
+        margin: EdgeInsets.symmetric(vertical: 8,horizontal: 16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(32),
+              topRight: Radius.circular(32),
+              bottomLeft: Radius.circular(32)
+          ),
+          color: Colors.orange,
+        ),
+        child: Text(message.message,style: TextStyle(
           color: Colors.white,
         ),),
       ),
